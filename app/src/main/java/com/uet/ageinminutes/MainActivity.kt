0de7f5285ff124ab56dcd64586e153ab7ac5603c
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2022-04-30 22:23:24 +0500

    Project Done..!

--- app/src/main/java/com/uet/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/uet/ageinminutes/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.uet.ageinminutes
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvSelectedDate: TextView? = null
+    private var tvAgeInDays: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ids
+        val btnDatePicker = findViewById<Button>(R.id.btnDatePicker)
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
+        tvAgeInDays = findViewById(R.id.tvAgeInDays)
+
+        // btn for date selection.
+        btnDatePicker.setOnClickListener {
+            clickDatePicker()
+        }
+    }
+
+    private fun clickDatePicker() {
+        val myCal = Calendar.getInstance()
+        val y = myCal.get(Calendar.YEAR)
+        val m = myCal.get(Calendar.MONTH)
+        val day = myCal.get(Calendar.DAY_OF_MONTH)
+        val dpd = DatePickerDialog(
+            this,
+            { _, year, month, dayOfMonth ->
+                val selectedDate = "$dayOfMonth-${month+1}-$year"
+                tvSelectedDate?.text = selectedDate
+
+                val sdf = SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH)
+                val formattedSelectedDate = sdf.parse(selectedDate)
+                val selectedDateInTime = formattedSelectedDate?.time?.div(86400000)
+                val todayDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                val todayDateInTime = todayDate?.time?.div(86400000)
+
+                val ageInDays = todayDateInTime?.minus(selectedDateInTime!!)
+                tvAgeInDays?.text = ageInDays.toString()
+            },
+            y,
+            m,
+            day,
+        )
+        dpd.datePicker.maxDate = System.currentTimeMillis()
+        dpd.show()
     }
 }
